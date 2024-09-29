63e78918a78f9dd5376ec73f551b5c2acb5e508a
shafiqalshaar <shafiqalshaar@gmail.com>
2024-09-29 16:46:16 +0300

    Change description size

---
+++ database/migrations/2024_09_29_134534_change_description_to_big_size_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('cars', function (Blueprint $table) {
+            $table->longText('description')->change();
+            $table->longText('description2')->change();
+            $table->longText('at_description')->change();
+            $table->longText('at_description2')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('cars', function (Blueprint $table) {
+            //
+        });
+    }
+};
